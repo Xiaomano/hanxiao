5c0d7fc86b750f4ad767532e0551553860eceabd
Hanxioa318 <18237302841@163.com>
2018-02-26 19:29:13 +0800

    111

---
+++ Hanxiao/Hanxiao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hanxiao
+//
+//  Created by Xiaoxiao_Mac on 2018/2/26.
+//  Copyright © 2018年 LinYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Hanxiao/HanxiaoTests/HanxiaoTests.m
@@ -0,0 +1,39 @@
+//
+//  HanxiaoTests.m
+//  HanxiaoTests
+//
+//  Created by Xiaoxiao_Mac on 2018/2/26.
+//  Copyright © 2018年 LinYun. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HanxiaoTests : XCTestCase
+
+@end
+
+@implementation HanxiaoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
